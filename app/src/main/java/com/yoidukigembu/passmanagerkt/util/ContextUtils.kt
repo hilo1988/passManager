d3f24ce7955b4eb8246e757fd390e4a5946ccb28
hilo <yoidukigembu@gmail.com>
2017-12-05 00:28:41 +0900

    stringformat変更

--- app/src/main/java/com/yoidukigembu/passmanagerkt/util/ContextUtils.kt
+++ app/src/main/java/com/yoidukigembu/passmanagerkt/util/ContextUtils.kt
@@ -1,20 +1,24 @@
 package com.yoidukigembu.passmanagerkt.util
 
 import com.yoidukigembu.passmanagerkt.PMApplication
+import org.apache.commons.lang3.ArrayUtils
 
+import java.util.ArrayList
 
-/**
- * コンテキストユーティリティ
- */
 object ContextUtils {
 
     /**
      * @param stringId フォーマットベースになるStringID
      * @param args フォーマットの引数になるStringID
      */
-    fun formatString(stringId:Int, vararg args:Int) : String {
-        val context = PMApplication.getContext()
-        val strings = args.map { s -> context.getString(s) }
-        return context.getString(stringId, strings.toTypedArray())
+    fun formatString(stringId: Int, vararg args: Int): String {
+
+        if (ArrayUtils.isEmpty(args)) {
+            return PMApplication.getContext().getString(stringId)
+        }
+        val values = args.map { id -> PMApplication.getContext().getString(id) }
+
+        return PMApplication.getContext().getString(stringId, *values.toTypedArray())
+
     }
 }
