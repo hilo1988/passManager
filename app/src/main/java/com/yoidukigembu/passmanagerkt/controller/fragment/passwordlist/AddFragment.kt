e4b5743e3b8032d4a9edf2e89be31a4ba2333b53
hilo <yoidukigembu@gmail.com>
2018-09-12 00:49:52 +0900

    パスワード入力部分を一部変更

--- app/src/main/java/com/yoidukigembu/passmanagerkt/controller/fragment/passwordlist/AddFragment.kt
+++ app/src/main/java/com/yoidukigembu/passmanagerkt/controller/fragment/passwordlist/AddFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.yoidukigembu.passmanagerkt.R
 import com.yoidukigembu.passmanagerkt.controller.fragment.BaseFragment
+import kotlinx.android.synthetic.main.fragment_app_password.*
 
 class AddFragment : BaseFragment() {
 
@@ -14,7 +15,12 @@ class AddFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater?.inflate(R.layout.fragment_password_input, null)
-        
+        return view!!
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        submitButton.setOnClickListener()
     }
 
     companion object {
